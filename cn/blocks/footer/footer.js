93380d03555dfa24e09b324e6103b174072bff15
David Nuescheler <david@nuescheler.ch>
2022-02-25 12:25:30 -0800

    chore: move to cn

--- cn/blocks/footer/footer.js
+++ cn/blocks/footer/footer.js
@@ -1,12 +1,15 @@
+import { decorateIcons } from '../../scripts/scripts.js';
+
 /**
  * loads and decorates the footer
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
-  const resp = await fetch('/footer.plain.html');
+  const resp = await fetch(`${window.hlx.codeBasePath}/footer.plain.html`);
   const html = await resp.text();
   const footer = document.createElement('div');
   footer.innerHTML = html;
+  decorateIcons(footer);
   const sections = ['brand', 'links', 'social', 'rights'];
   sections.forEach((e, i) => {
     footer.children[i].classList.add(e);
