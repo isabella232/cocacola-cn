8e7a8a6842a8561a967d402bcc557c1c537ac4de
David Nuescheler <david@nuescheler.ch>
2022-02-25 14:48:56 -0800

    chore: add gtm

--- cn/scripts/scripts.js
+++ cn/scripts/scripts.js
@@ -755,4 +755,8 @@ async function loadLazy(doc) {
  */
 function loadDelayed() {
   // load anything that can be postponed to the latest here
+  setTimeout(() => {
+    // eslint-disable-next-line
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N6GZQ6');
+  }, 4000);
 }
